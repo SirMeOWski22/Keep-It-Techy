6aab2dae9707fdac1920d300c365cb14a5bf6724
Morgan <160355750+SirMeOWski22@users.noreply.github.com>
2024-07-13 22:49:03 -0500

    Update connection.js

--- config/connection.js
+++ config/connection.js
@@ -1,11 +1,22 @@
 const Sequelize = require('sequelize');
 require('dotenv').config();
 
-const sequelize = new Sequelize(process.env.DB_NAME, process.env.DB_USER, process.env.DB_PASSWORD, {
-  host: process.env.DB_HOST,
-  dialect: process.env.DB_DIALECT || 'postgres',
-  port: process.env.DB_PORT || 5432,
-  }
-);
+const sequelize = process.env.DATABASE_URL
+  ? new Sequelize(process.env.DATABASE_URL, {
+      dialect: 'postgres',
+      protocol: 'postgres',
+      logging: false, // Disable logging; default: console.log
+    })
+  : new Sequelize(
+      process.env.DB_NAME,
+      process.env.DB_USER,
+      process.env.DB_PASSWORD,
+      {
+        host: process.env.DB_HOST,
+        dialect: process.env.DB_DIALECT || 'postgres',
+        port: process.env.DB_PORT || 5432,
+      }
+    );
 
 module.exports = sequelize;
+
